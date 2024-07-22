57215bbf18a2b5422469ad0e151152d421bb2726
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-23 04:13:14 +0530

    Updated styling

--- vite-admin/src/pages/Dashboard.jsx
+++ vite-admin/src/pages/Dashboard.jsx
@@ -1,24 +1,24 @@
-// src/pages/Dashboard.js
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 import AddCategory from '../components/AddCategory';
 import AddMovie from '../components/AddMovie';
 import ShowtimeManagement from '../components/ShowtimeManagement';
 import BookedMovies from '../components/BookedMovie';
 import { useAuth } from '../components/AuthContext';
+import './Dashboard.css'
 
 const Dashboard = () => {
   const { currentUser } = useAuth(); 
 
   return (
     <div className="dashboard-container">
-      <div style={{marginLeft:'33rem'}}>
+      <div className='welcome'>
       <h2>Welcome Admin Piyush</h2>
       <p>Email: {currentUser.email}</p>
       </div>
       {/* <Sidebar /> */}
-      <div className="dashboard-content">
+      <div>
         <Routes>
           <Route path="add-category" element={<AddCategory />} />
           <Route path="add-movie" element={<AddMovie />} />
