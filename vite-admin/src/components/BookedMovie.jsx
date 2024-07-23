b16692846f3c927bff7e05be5a1a704564b8b5a3
immortalWebDev <pgbadgujar007@gmail.com>
2024-07-24 00:17:23 +0530

    Added loading state, Improved list show (Latest booking first)

--- vite-admin/src/components/BookedMovie.jsx
+++ vite-admin/src/components/BookedMovie.jsx
@@ -1,39 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
-import './BookedMovies.css'; 
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import "./BookedMovies.css";
 
 const BookedMovies = () => {
   const [bookedMovies, setBookedMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchBookedMovies = async () => {
-      const querySnapshot = await getDocs(collection(db, 'bookings'));
-      setBookedMovies(querySnapshot.docs.map(doc => doc.data()));
+      setLoading(true);
+      try {
+        const querySnapshot = await getDocs(collection(db, "bookings"));
+        const bookings = querySnapshot.docs.map((doc) => doc.data());
+        bookings.sort((a, b) => b.timestamp - a.timestamp); 
+        setBookedMovies(bookings);
+        
+      } catch (error) {
+        console.error("Error fetching booked movies: ", error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchBookedMovies();
+    // console.log(bookedMovies)
   }, []);
 
   return (
     <div className="booked-movies-container">
       <h2>Booked Movies</h2>
-      <ul>
-        {bookedMovies.map((booking, index) => (
-          <li key={index}>
-            <strong>{booking.name}</strong> ({booking.email}) booked <em>{booking.movieName}</em> at <em>{booking.showtime}</em>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p style={{textAlign:'center'}}>Fetching Booking Details...</p>
+      ) : (
+        <ul>
+          {bookedMovies.map((booking, index) => (
+            <li key={index}>
+              <span style={{fontWeight:"bold"}}>{booking.name}</span> ({booking.email}) booked{" "}
+              <strong><em>{booking.movieName}</em></strong> at <span style={{fontWeight:'bold'}}>{booking.showtime}</span> for <span style={{fontWeight:'bold'}}>{booking.date}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
 export default BookedMovies;
-
-
-
-
-
-
-
 
